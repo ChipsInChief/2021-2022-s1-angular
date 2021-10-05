16d3408e781068f07fa491687855a7719c4b34a6
Benjamin GUILLAUME <benjamin.guillaume@syspertec.com>
2021-10-05 08:27:26 +0200

    Création du premier composant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MonPremierComponent } from './mon-premier/mon-premier.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonPremierComponent
   ],
   imports: [
     BrowserModule
