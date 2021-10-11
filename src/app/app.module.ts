afe8c652d41121a3640446111365f5644de8f6a2
Benjamin GUILLAUME <benjamin.guillaume@syspertec.com>
2021-10-11 19:40:33 +0200

    Ajout du composant appareil

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MonPremierComponent } from './mon-premier/mon-premier.component';
+import { AppareilComponent } from './appareil/appareil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MonPremierComponent
+    MonPremierComponent,
+    AppareilComponent
   ],
   imports: [
     BrowserModule
