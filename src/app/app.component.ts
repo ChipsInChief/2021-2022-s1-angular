0756f49b57284a1b351f9bcd123c83dfe4c61044
Benjamin GUILLAUME <benjamin.guillaume@syspertec.com>
2021-10-11 21:01:45 +0200

    Property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Ma Super Application';
+  isAuth = false;
+
+  constructor() {
+    setTimeout(
+      () => { this.isAuth = true; },
+      4000
+    );
+  }
 }
