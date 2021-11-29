96c20f861dac6c2d75d6c345fc4830340b395ce4
ChipsInChief <ouri3l@gmail.com>
2021-11-29 20:42:53 +0100

    👌 IMPROVE: Déplacement de la logique et du template de AppComponent vers AppareilViewComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,39 +7,13 @@ import { AppareilService } from './services/appareil.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  isAuth = false;
-  lastUpdate:Promise<Date> = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
+  
+  constructor() {
 
-  appareils?: any[];
-
-  constructor(private appareilService: AppareilService) {
-    setTimeout(
-      () => { this.isAuth = true; },
-      4000
-    );
   }
 
   ngOnInit(): void {
-    this.appareils = this.appareilService.appareils;
+    
   }
-
-  onAllumer(){
-    this.appareilService.switchOnAll();
-  }
-  onEteindre() {
-    if (confirm('Êtes-vous sûr de vouloir éteindre tous vos appareils ?')) {
-      this.appareilService.switchOffAll();
-    } else {
-      return null;
-    }
-
-    return true;
-  }
+ 
 }
--- src/app/appareil-view/appareil-view.component.ts
+++ src/app/appareil-view/appareil-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AppareilService } from '../services/appareil.service';
 
 @Component({
   selector: 'app-appareil-view',
@@ -6,10 +7,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./appareil-view.component.scss']
 })
 export class AppareilViewComponent implements OnInit {
+  isAuth = false;
+  lastUpdate: Promise<Date> = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+  appareils?: any[];
 
-  constructor() { }
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => { this.isAuth = true; },
+      4000
+    );
+  }
 
   ngOnInit(): void {
+    this.appareils = this.appareilService.appareils;
+  }
+
+  onAllumer() {
+    this.appareilService.switchOnAll();
+  }
+  onEteindre() {
+    if (confirm('Êtes-vous sûr de vouloir éteindre tous vos appareils ?')) {
+      this.appareilService.switchOffAll();
+    } else {
+      return null;
+    }
+
+    return true;
   }
 
 }
