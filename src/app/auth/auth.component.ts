5c5c11898d41b9f2225b5c7271585bf3502e76d0
ChipsInChief <ouri3l@gmail.com>
2021-12-06 19:45:52 +0100

    👌 IMPROVE: Ajout de la méthode onsignIn

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -16,6 +16,15 @@ export class AuthComponent implements OnInit {
     this.authStatus = this.authService.isAuth;
   }
 
+  onSignIn(){
+    this.authService.signIn().then(
+      () => {
+        console.log('Sign in successful!');
+        this.authStatus = this.authService.isAuth;
+      }
+    )
+  }
+
   onSignOut(){
     this.authService.signOut();
     this.authStatus = this.authService.isAuth;
